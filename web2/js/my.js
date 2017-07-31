70ac56caf7122ac4440997163bc09730bd0df411
akong0115 <1099484683@qq.com>
2017-07-31 13:55:52 +0800

    Add files via upload

---
+++ web2/js/my.js
@@ -0,0 +1,44 @@
+function gdjz(div,cssname,offset){
+    var a,b,c,d;
+    d=$(div).offset().top;
+    a=eval(d + offset);
+    b=$(window).scrollTop(); //监控窗口已滚动的距离;
+    c=$(window).height();//浏览器窗口的高度*/
+    console.log(a,b,c,d);
+
+    if(b>=offset){
+    console.log("add");
+        $(div).addClass(cssname);
+     } else if (b<offset) {
+        console.log("remove");
+        $(div).removeClass(cssname);
+     }
+}
+$(document).ready(function(){
+    $('#name').fadeIn(2000);
+    var rallax=new Rellax('.slow');
+    var rallax=new Rellax('fast');
+    // var word=document.getElementsByTagName('.dercription h2');
+    var svg1 = new Vivus('svg1', {type: 'oneByOne', duration: 100});
+    // var svg2 = new Vivus('svg2', {type: 'oneByOne', duration: 100});
+
+});
+function svglocation(div,offset){
+    var a,b,c,d;
+    // d=$(div).offset().top;
+    a=eval(d);
+    b=$(window).scrollTop(); //监控窗口已滚动的距离;
+    c=$(window).height();//浏览器窗口的高度*/
+    // console.log(a,b,c,d);
+    if(b>offset){
+    var myVivus=new Vivus(div).play();
+     }
+}
+$(window).scroll(function(){
+    gdjz('#title','animated bounceInLeft',200);
+    gdjz('.dercription>ul li','animated fadeInUp',250);
+    gdjz('#word2 h2','animated bounceInRight',700);
+    gdjz('#leftPhone','animated bounceInRight',2500);
+    gdjz('#rightPhone','animated bounceInLeft',2500);
+    gdjz('#centerPhone','animated fadeInDown',2500);
+})
