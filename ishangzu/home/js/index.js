ffd1e0f22a1a6ec0f746a5de62877eb593c3a818
akong0115 <1099484683@qq.com>
2018-05-13 14:08:48 +0800

    增加侧边栏js

--- ishangzu/home/js/index.js
+++ ishangzu/home/js/index.js
@@ -1,29 +1,106 @@
-document.getElementById('drop-down-select').onclick = function (event) {
-  event.stopPropagation();
-  // console.log(this.children[1]);
-  if (this.children[1].className === "iconfont icon-arrow-down") {
-    this.children[1].className = "iconfont icon-arrow-up";
-    document.getElementById('city-select').className = 'show drop-select';
-  } else {
-    this.children[1].className = "iconfont icon-arrow-down";
+window.onload = function (e) {
+  // 点击body收回下拉列表
+  document.body.onclick = function (e) {
+    event.stopPropagation();
     document.getElementById('city-select').className = 'drop-select';
+    document.getElementById('drop-down-select').children[1].className = "iconfont icon-arrow-down";
+  }
+  // 城市选择
+  document.getElementById('drop-down-select').onclick = function (event) {
+    event.stopPropagation();
+    if (this.children[1].className === "iconfont icon-arrow-down") {
+      this.children[1].className = "iconfont icon-arrow-up";
+      document.getElementById('city-select').className = 'show drop-select';
+      for (let i = 0; i < 4; i++) {
+        // console.log(document.getElementById('city-select').children[i]);
+        // console.log(this);
+        document.getElementById('city-select').children[i].onclick=function(e){
+          // console.log(this);
+          // console.log(this.parentElement.parentElement.children[0].children[0].innerHTML);
+          this.parentElement.parentElement.children[0].children[0].innerHTML=this.innerHTML;
+          // console.log(this.parentElement.childElementCount);
+          for(let i=0;i<this.parentElement.childElementCount;i++){
+            // console.log(this.parentElement.children[i]);
+            this.parentElement.children[i].className='';
+          }
+          this.className='active-selected';
+        }
+      }
+    } else {
+      this.children[1].className = "iconfont icon-arrow-down";
+      document.getElementById('city-select').className = 'drop-select';
+    }
+  }
+
+  // 搜索
+  function areaSeltctShow(event) {
+    event.stopPropagation();
+    if (this.parentElement.children[1].className === 'drop-select') {
+      this.parentElement.children[1].className = 'drop-select show';
+      for(let i=0;i<this.parentElement.children[1].childElementCount;i++){
+        this.parentElement.children[1].children[i].onclick=function(e){
+          // console.log(this.parentElement.parentElement.children[0]);
+          this.parentElement.parentElement.children[0].value=this.innerHTML;
+        }
+      }
+    } else {
+      this.parentElement.children[1].className = 'drop-select';
+    }
+  }
+  document.getElementById('area-input').onmouseover = areaSeltctShow;
+  document.getElementById('area-input').onmouseout = areaSeltctShow;
+  document.getElementById('area-select').onmouseover = function (e) {
+    event.stopPropagation();
+    this.parentElement.children[1].className = 'drop-select show';
+  }
+  document.getElementById('area-select').onmouseout = function (e) {
+    event.stopPropagation();
+    this.parentElement.children[1].className = 'drop-select';
+  }
+}
+// 侧边栏
+document.getElementById('entrust').onmousemove=function(e){
+  e.stopPropagation();
+  // console.log(this);
+  this.children[0].className='sidebar-show';
+}
+document.getElementById('entrust').onmouseout=function(e){
+  e.stopPropagation();
+  // console.log(this);
+  this.children[0].className='sidebar-show hide';
+
+}
+document.getElementById('qr-code').onmouseover=function (e) {
+  e.stopPropagation();
+  this.children[0].className='sidebar-show';
 
   }
-}
-console.log(document.getElementsByClassName('search'));
-function areaSeltctShow(event) {
-  event.stopPropagation();
-  if (this.parentElement.children[1].className === 'drop-select') {
-    this.parentElement.children[1].className = 'drop-select show';
-  } else {
-    this.parentElement.children[1].className = 'drop-select';
+  document.getElementById('qr-code').onmouseout=function(e){
+    e.stopPropagation();
+    // console.log(this);
+    this.children[0].className='sidebar-show hide';
+  
   }
-}
-document.getElementById('area-input').onmouseover = areaSeltctShow;
-document.getElementById('area-input').onmouseout = areaSeltctShow;
-document.getElementById('area-select').onmouseover=function (e) {
-  this.parentElement.children[1].className = 'drop-select show';
-  }
-  document.getElementById('area-select').onmouseout=function (e) {
-    this.parentElement.children[1].className = 'drop-select';
+  // 滚动
+  window.onscroll=function(e){
+    // this.console.log(document.documentElement.scrollTop);
+    let t=document.documentElement.scrollTop||document.body.scrollTop;
+    if(t>150){
+      // console.log(`66`);
+      document.getElementById('up-to-head').className='trans-hide slow-show iconfont icon-arrow-up';
+    }else{
+      document.getElementById('up-to-head').className='iconfont icon-arrow-up trans-hide';
     }
+  }
+  document.getElementById('up-to-head').onmouseover=function(e){
+    if(document.getElementById('up-to-head').className==='trans-hide slow-show iconfont icon-arrow-up')
+    // console.log(document.getElementById('up-to-head').style.visibility);
+    document.getElementById('up-top-show').className='sidebar-show';
+  }
+  document.getElementById('up-to-head').onmouseout=function(e){
+    document.getElementById('up-top-show').className='sidebar-show hide';
+  }
+  document.getElementById('up-to-head').onclick=function(e){
+    document.documentElement.scrollTop=0;
+    document.getElementById('up-top-show').className='sidebar-show hide';
+  }
