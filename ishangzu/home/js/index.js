ed5dcb5d5fa1e609eb22910aac60438adcb5ebb8
akong0115 <1099484683@qq.com>
2018-05-29 14:37:59 +0800

    Revert "add mobile home"
    This reverts commit 38b0b0a83c213b6c9ba6ffd99243a82cdb2455ab.

--- ishangzu/home/js/index.js
+++ ishangzu/home/js/index.js
@@ -125,18 +125,24 @@ document.getElementById('up-to-head').onclick = function (e) {
 }
 // console.log(document.getElementById('band-house-btns').children);
 // 品牌按钮
-let slideShowInterval;
-function slide(className, num) {
-  slideShowInterval = setInterval(function () {
-    nextImg(className, num);
-  }, 5000);
-};
-slide('slideshow', 0);
-slide('slideshow', 3);
+// let slideShowInterval ;
+// function slide(className, num) {
+//   slideShowInterval= setInterval(function () {
+//     nextImg(className, num);
+//   }, 5000);
+// };
+// slide('slideshow',0);
+// slide('slideshow',3);
 for (let i = 1; i < 4; i++) {
   document.getElementById('band-house-btns').children[i].onclick = function (e) {
     e.stopPropagation();
-    slide('slideshow', i - 1);
+    // slide('slideshow', i-1);
+    getEl(i - 1);
+    clearInterval(timer);
+    pic = fk = 0;
+    timer = setInterval(function () {
+      rightArrs[i - 1].onclick();
+    }, 1000);
     console.log(this.parentElement);
     for (let j = 1; j < 4; j++) {
       this.parentElement.children[j].className = '';
@@ -223,97 +229,245 @@ for (let i = 0; i < dots.length; i++) {
   }
 }
 //页面轮播图
-let dotNum = 0;
+// let dotNum = 0;
 
-function nextImg(className, i) {
-  let slideShow = document.getElementsByClassName(className)[i];
-  // console.log(slideShow);
-  let left = parseInt(slideShow.children[0].style.left);
-  let timer = null;
-  let flag = 0;
-  let slideDots = document.getElementsByClassName('img-dot');
-  // console.log(slideDots[1].childElementCount);
-  for (let j = 0; j < slideDots[i + 1].childElementCount; j++) {
-    slideDots[i + 1].children[j].className = 'iconfont';
+// function nextImg(className, i) {
+//   let slideShow = document.getElementsByClassName(className)[i];
+//   // console.log(slideShow);
+//   let left = parseInt(slideShow.children[0].style.left);
+//   let timer = null;
+//   let flag = 0;
+//   let slideDots = document.getElementsByClassName('img-dot');
+//   // console.log(slideDots[1].childElementCount);
+//   for (let j = 0; j < slideDots[i + 1].childElementCount; j++) {
+//     slideDots[i + 1].children[j].className = 'iconfont';
+//   }
+//   console.log(dotNum);
+//   if (dotNum < 2) {
+//     dotNum++;
+//   } else {
+//     dotNum = 0;
+//   }
+//   timer = setInterval(function () {
+//     if (left > -2280) {
+//       if (flag < 570) {
+//         left -= 10;
+//         flag += 10
+//         slideShow.children[0].style.left = `${left}px`;
+//       }
+//       else {
+//         flag = 0;
+//         slideDots[i + 1].children[dotNum].className = 'iconfont active-dot';
+//         clearInterval(timer);
+
+//       }
+//     } else {
+//       left = -570;
+//       slideShow.children[0].style.left = `${left}px`;
+//       flag = 0;
+//       slideDots[i + 1].children[0].className = 'iconfont active-dot';
+//       clearInterval(timer);
+
+//     }
+//   }, 10);
+// }
+// function prevImg(className, i) {
+//   let slideShow = document.getElementsByClassName(className)[i];
+//   let left = parseInt(slideShow.children[0].style.left);
+//   let timer = null;
+//   let flag = 0;
+//   let slideDots = document.getElementsByClassName('img-dot');
+//   for (let j = 0; j < slideDots[i + 1].childElementCount; j++) {
+//     slideDots[i + 1].children[j].className = 'iconfont';
+//   }
+//   if (dotNum >0) {
+//     dotNum--;
+//   } else {
+//     dotNum = 2;
+//   }
+//   timer = setInterval(function () {
+//     if (left < 0) {
+//       if (flag < 570) {
+//         left += 10;
+//         flag += 10
+//         slideShow.children[0].style.left = `${left}px`;
+//       }
+//       else {
+//         flag=0;
+//         slideDots[i + 1].children[dotNum].className = 'iconfont active-dot';
+//         clearInterval(timer);
+//       }
+//     } else {
+//       left = -1710;
+//       slideShow.children[0].style.left = `${left}px`;
+//       slideDots[i + 1].children[2].className = 'iconfont active-dot';
+//       flag = 0;
+//       clearInterval(timer);
+//     }
+//   }, 10);
+// }
+
+
+
+// let slidePrevs = document.getElementsByClassName('slide-prev');
+// for (let i = 0; i < slidePrevs.length; i++) {
+//   slidePrevs[i].onclick = function (e) {
+//     e.stopPropagation();
+//     prevImg(`slideshow`, i);
+//   }
+// }
+// let slideNexts = document.getElementsByClassName('slide-next');
+// for (let i = 0; i < slideNexts.length; i++) {
+//   slideNexts[i].onclick = function (e) {
+//     e.stopPropagation();
+//     nextImg(`slideshow`, i);
+//   }
+// }
+//动画函数
+function animate(el, target) {
+  if (el.timer) {
+    clearInterval(el.timer);
   }
-  console.log(dotNum);
-  if (dotNum < 2) {
-    dotNum++;
-  } else {
-    dotNum = 0;
-  }
-  timer = setInterval(function () {
-    if (left > -2280) {
-      if (flag < 570) {
-        left -= 10;
-        flag += 10
-        slideShow.children[0].style.left = `${left}px`;
-      }
-      else {
-        flag = 0;
-        slideDots[i + 1].children[dotNum].className = 'iconfont active-dot';
-        clearInterval(timer);
-
-      }
-    } else {
-      left = -570;
-      slideShow.children[0].style.left = `${left}px`;
-      flag = 0;
-      slideDots[i + 1].children[0].className = 'iconfont active-dot';
-      clearInterval(timer);
-
+  el.timer = setInterval(function () {
+    //leader = leader + step
+    var leader = el.offsetLeft;
+    var step = 30;
+    if (target < leader) {
+      step = -step;
     }
-  }, 10);
-}
-function prevImg(className, i) {
-  let slideShow = document.getElementsByClassName(className)[i];
-  let left = parseInt(slideShow.children[0].style.left);
-  let timer = null;
-  let flag = 0;
-  let slideDots = document.getElementsByClassName('img-dot');
-  for (let j = 0; j < slideDots[i + 1].childElementCount; j++) {
-    slideDots[i + 1].children[j].className = 'iconfont';
-  }
-  if (dotNum > 0) {
-    dotNum--;
-  } else {
-    dotNum = 2;
-  }
-  timer = setInterval(function () {
-    if (left < 0) {
-      if (flag < 570) {
-        left += 10;
-        flag += 10
-        slideShow.children[0].style.left = `${left}px`;
-      }
-      else {
-        flag = 0;
-        slideDots[i + 1].children[dotNum].className = 'iconfont active-dot';
-        clearInterval(timer);
-      }
+    //如果到达终点的距离已经小于一步了，就直接跨到终点。
+    if (Math.abs(target - leader) >= Math.abs(step)) {
+      leader = leader + step;
+      el.style.left = leader + "px";
     } else {
-      left = -1710;
-      slideShow.children[0].style.left = `${left}px`;
-      slideDots[i + 1].children[2].className = 'iconfont active-dot';
-      flag = 0;
-      clearInterval(timer);
+      clearInterval(el.timer);
+      el.style.left = target + "px";
     }
-  }, 10);
+  }, 15);
 }
-
-
-
-let slidePrevs = document.getElementsByClassName('slide-prev');
-for (let i = 0; i < slidePrevs.length; i++) {
-  slidePrevs[i].onclick = function (e) {
-    e.stopPropagation();
-    prevImg(`slideshow`, i);
+//1. 找对象
+var box;
+var slideshow=document.getElementsByClassName("slideshow")[0];
+var ul;
+var ullis;
+var ol;
+var arr;
+var leftArrs = document.getElementsByClassName("slide-prev");
+var rightArrs = document.getElementsByClassName("slide-next");
+var imgwidth = slideshow.offsetWidth;
+var pic = fk = 0;
+var timer;
+var ollis;
+function getEl(num) {
+  box = document.getElementsByClassName("all")[num];
+  slideshow = document.getElementsByClassName("slideshow")[num];
+  ul = slideshow.children[0];
+  ullis = ul.children;
+  ol = slideshow.children[1];
+  arr = document.getElementsByClassName('slide-arr')[num];
+  console.log(slideshow);
+  console.log(imgwidth);
+  pic = fk = 0;
+  //2. 动态创建结构
+  //2.1 创建小方块,ulLis
+  //根据ul中li的个数创建小方块
+  if (ol.childElementCount == 0)
+    for (var i = 0; i < ullis.length; i++) {
+      var li = document.createElement("li");
+      ol.appendChild(li);
+      li.innerHTML = '&#xe608';
+      li.className = 'iconfont';
+    }
+  ollis = ol.children;
+  ollis[0].className = "iconfont active-dot";
+  //2.2 创建假图片
+  //2.2.1 克隆ul下的第一个li
+  var cloneli = ullis[0].cloneNode(true);
+  ul.appendChild(cloneli);
+  //3. 简单轮播功能
+  //3.1 给小方块注册点击事件
+  for (var i = 0; i < ollis.length; i++) {
+    ollis[i].index = i; //存索引
+    ollis[i].addEventListener("click", function () {
+      //3.2 小方块高亮排他
+      for (var i = 0; i < ollis.length; i++) {
+        ollis[i].className = "iconfont";
+      }
+      this.className = "iconfont active-dot";
+      //3.3. 移动ul
+      var target = -this.index * imgwidth;
+      animate(ul, target);
+      pic = fk = this.index;
+    })
+  }
+  //4. 左右焦点功能（无缝）
+  //4.1 鼠标经过盒子，显示箭头
+  box.onmouseover = function () {
+    arr.style.display = "block";
+    //清除定时器
+    clearInterval(timer);
+    clearInterval(timer2);
+  }
+  //4.2 鼠标离开盒子，隐藏箭头
+  box.onmouseleave = function () {
+    arr.style.display = "none";
+    timer = setInterval(function () {
+      rightArrs[num + 1].onclick();
+    }, 1000)
   }
 }
-let slideNexts = document.getElementsByClassName('slide-next');
-for (let i = 0; i < slideNexts.length; i++) {
-  slideNexts[i].onclick = function (e) {
-    e.stopPropagation();
-    nextImg(`slideshow`, i);
+//4.3 点击右箭头
+for (let i = 0; i < rightArrs.length; i++) {
+  rightArrs[i].onclick = function () {
+    //如果已经到了最后一张假图片，让ul瞬移到第一张真图片
+    if (pic === ollis.length) {
+      ul.style.left = 0;
+      pic = 0;
+    }
+    pic++; //记录出去的图片张数
+    fk++;
+    if (fk === ollis.length) {
+      fk = 0;
+    }
+    for (var i = 0; i < ollis.length; i++) {
+      ollis[i].className = "iconfont";
+    }
+    ollis[fk].className = "iconfont active-dot";
+    var target = -pic * imgwidth;
+    animate(ul, target);
   }
 }
+//4.4 点击左箭头
+for (let i = 0; i < leftArrs.length; i++) {
+  leftArrs[i].onclick = function () {
+    if (pic === 0) {
+      ul.style.left = -(ullis.length - 1) * imgwidth + "px";
+      pic = ullis.length - 1;
+    }
+    pic--;
+    //同步小方块
+    fk--;
+    if (fk === -1) {
+      fk = ollis.length - 1;
+    }
+    for (var i = 0; i < ollis.length; i++) {
+      ollis[i].className = "iconfont";
+    }
+    ollis[fk].className = "iconfont active-dot";
+    var target = -pic * imgwidth;
+    animate(ul, target);
+  }
+}
+//5. 自动播放的功能
+getEl(0);
+timer = setInterval(function () {
+  rightArrs[0].onclick();
+}, 1000);
+getEl(3);
+timer2=setInterval(function () {
+  rightArrs[3].onclick();
+}, 1000);;
+//6. 同步问题
+//6.1 点击右箭头,同步
+//6.2 点击左箭头，同步
+//6.3 点击小方块，同步
